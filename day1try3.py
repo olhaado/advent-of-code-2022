5fc02eebdfd9c2ee4e9cd1404abf2aa5b05736fe
olhaado <118556396+olhaado@users.noreply.github.com>
2022-12-06 22:23:41 -0500

    day 1 part 2 solve

--- day1try3.py
+++ day1try3.py
@@ -16,10 +16,34 @@ def part1(file):
         else:
             cals = 0
     return maxCals
+    file.close()
 
-def checkType(file):
+def part2(file):
+    
+    cals = 0
+    firstElf = 0
+    secondElf = 0
+    thirdElf = 0
+    
     for i in file:
-        print(type(i))
+        if i not in['\n', '\r', '\r\n']:
+            cals += int(i)
+            print(int(i))
+        else:
+            print("no")
+            if cals > firstElf:
+                print("yes")
+                thirdElf = secondElf
+                secondElf = firstElf
+                firstElf = cals
+                print(f"{firstElf},{secondElf},{thirdElf}")
+            cals = 0
+    top3 = [firstElf, secondElf, thirdElf]
+    return top3
+    file.close()
 
-answer = part1(file)
-print(answer)
+#answer = part1(file)
+#print(answer)
+
+answer = part2(file)
+print(sum(answer))
